b4db4522fac428eb2f8006ce04fcb2d1c2390eef
Luis Garcia <luigi311.lg@gmail.com>
2017-11-20 22:50:32 -0700

    Make items appear in a column by name

--- django/compatability/views.py
+++ django/compatability/views.py
@@ -8,7 +8,14 @@ from django.views.generic import TemplateView, DetailView, ListView, CreateView 
 from .models import Motherboard, Cpu, Memory
 
 class HomeView(TemplateView):
+
     template_name='home.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['motherboard'] = Motherboard.objects.all()
+        context['cpu'] = Cpu.objects.all()
+        context['memory'] = Memory.objects.all()
+        return context
 
 class AboutView(TemplateView):
     template_name='about.html'
